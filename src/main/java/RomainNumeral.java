8fdd079150928e26216fbb31ce9f5f63e3198be0
Henri LAY <h.lay@gop.link>
2018-11-15 19:08:40 +0100

    second round

--- src/main/java/RomainNumeral.java
+++ src/main/java/RomainNumeral.java
@@ -9,15 +9,23 @@ public class RomainNumeral {
     symbols.put(1, "I");
     symbols.put(4, "IV");
     symbols.put(5, "V");
+    symbols.put(9, "IX");
+    symbols.put(10, "X");
   }
 
   public String convert(int arabicNumber) {
 
-    if (symbols.containsKey(arabicNumber)) {
-      return symbols.get(arabicNumber);
+    String result = "";
+
+    for (int i = arabicNumber; i >= 0; i--) {
+
+      if (symbols.containsKey(i)) {
+        result += symbols.get(i);
+        arabicNumber-=i;
+        break;
+      }
     }
 
-    String result = "";
     for (int i = 0; i < arabicNumber; i++) {
       result += "I";
     }
--- src/test/java/RomainNumeralShould.java
+++ src/test/java/RomainNumeralShould.java
@@ -41,4 +41,30 @@ public class RomainNumeralShould {
     assertThat(actual).isEqualTo("V");
   }
 
+  @Test
+  public void return_VI_when_input_6() {
+    RomainNumeral romainNumeral = new RomainNumeral();
+    String actual = romainNumeral.convert(6);
+    assertThat(actual).isEqualTo("VI");
+  }
+  @Test
+  public void return_VII_when_input_7() {
+    RomainNumeral romainNumeral = new RomainNumeral();
+    String actual = romainNumeral.convert(7);
+    assertThat(actual).isEqualTo("VII");
+  }
+
+  @Test
+  public void return_IX_when_input_9() {
+    RomainNumeral romainNumeral = new RomainNumeral();
+    String actual = romainNumeral.convert(9);
+    assertThat(actual).isEqualTo("IX");
+  }
+
+  @Test
+  public void return_X_when_input_10() {
+    RomainNumeral romainNumeral = new RomainNumeral();
+    String actual = romainNumeral.convert(10);
+    assertThat(actual).isEqualTo("X");
+  }
 }
